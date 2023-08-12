b447a1eded595aaf930e638c8a9924188bab5fae
lucasalb1 <lucasalb160@gmail.com>
2023-08-12 20:39:14 -0300

    .

--- assets/home.js
+++ assets/home.js
@@ -1,3 +1,19 @@
-let hora = document.querySelector('#horas')
-let minuto = document.querySelector('#minutos')
-let segundo = document.querySelector('#segundos')
+
+const hora = document.querySelector('#horas')
+const minuto = document.querySelector('#minutos')
+const segundo = document.querySelector('#segundos')
+
+const relogio = setInterval(function time(){ 
+    let dateToday = new Date();
+    let hor = dateToday.getHours();
+    let minut = dateToday.getMinutes();
+    let segund = dateToday.getSeconds();
+
+    if (hor < 10) hor = '0' + hor;
+    if (minut < 10) minut = '0' + minut;
+    if (segund < 10) segund = '0' + segund;
+
+    hora.textContent = hor;
+    minuto.textContent = minut;
+    segundo.textContent = segund;
+})
